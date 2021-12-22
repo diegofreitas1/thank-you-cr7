fc3ab4ff7a3380dcb9c7869cfaeef9d0aa35bc0d
diegofreitas1 <diegofreitas13@gmail.com>
2021-12-22 10:17:08 -0300

    Cr7 calm down

--- src/action.js
+++ src/action.js
@@ -12,7 +12,7 @@ async function run() {
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can. \n\n<img src="https://tenor.com/view/cr7-calm-down-its-me-gif-15038048" alt="thank you" /> '
   });
 }
   
