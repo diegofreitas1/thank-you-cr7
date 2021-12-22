eb2dd664c9887ba875c444d7c94c38191cf68be7
diegofreitas1 <diegofreitas13@gmail.com>
2021-12-22 10:15:49 -0300

    Cr7 calm down

--- dist/index.js
+++ dist/index.js
@@ -8445,7 +8445,9 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const { context } = __nccwpck_require__(5438)
+
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
 const octokit = github.getOctokit(GITHUB_TOKEN);
   
 const { pull_request } = context.payload;
@@ -8454,7 +8456,7 @@ async function run() {
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="https://tenor.com/view/cr7-calm-down-its-me-gif-15038048" alt="thank you" /> '
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
   });
 }
   
--- src/action.js
+++ src/action.js
@@ -1,7 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { context } = require('@actions/github')
+
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
 const octokit = github.getOctokit(GITHUB_TOKEN);
   
 const { pull_request } = context.payload;
@@ -10,7 +12,7 @@ async function run() {
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="https://tenor.com/view/cr7-calm-down-its-me-gif-15038048" alt="thank you" /> '
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
   });
 }
   
